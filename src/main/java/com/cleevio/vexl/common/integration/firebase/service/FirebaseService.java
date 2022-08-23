682bd136649611271d9406877bafe7ad92f15c38
David Tilšer <davidtilser@gmail.com>
2022-08-23 17:19:34 +0200

    VEX-816 Modify notifications into data notifications

--- src/main/java/com/cleevio/vexl/common/integration/firebase/service/FirebaseService.java
+++ src/main/java/com/cleevio/vexl/common/integration/firebase/service/FirebaseService.java
@@ -2,11 +2,10 @@ package com.cleevio.vexl.common.integration.firebase.service;
 
 import com.cleevio.vexl.module.push.dto.PushMessageDto;
 import com.cleevio.vexl.module.push.service.NotificationService;
+import com.google.firebase.messaging.AndroidConfig;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.Message;
-import com.google.firebase.messaging.Notification;
 import lombok.extern.slf4j.Slf4j;
-import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -18,20 +17,18 @@ public class FirebaseService implements NotificationService {
     private static final String TYPE = "type";
     private static final String INBOX = "inbox";
     private static final String SENDER = "sender";
-    private static final String DATA = "data";
 
     public void sendPushNotification(final PushMessageDto dto) {
         try {
             var messageBuilder = Message.builder();
 
-            messageBuilder.setNotification(Notification.builder().setTitle(dto.title()).setBody(dto.text()).build());
             messageBuilder.setToken(dto.token());
-            final String dataJson = new JSONObject()
-                    .put(TYPE, dto.messageType().name())
-                    .put(INBOX, dto.receiverPublicKey())
-                    .put(SENDER, dto.senderPublicKey())
-                    .toString();
-            messageBuilder.putData(DATA, dataJson);
+            messageBuilder.putData(TITLE, dto.title());
+            messageBuilder.putData(BODY, dto.text());
+            messageBuilder.putData(TYPE, dto.messageType().name());
+            messageBuilder.putData(INBOX, dto.receiverPublicKey());
+            messageBuilder.putData(SENDER, dto.senderPublicKey());
+            messageBuilder.setAndroidConfig(AndroidConfig.builder().build());
 
             final String response = FirebaseMessaging.getInstance().sendAsync(messageBuilder.build()).get();
             log.info("Sent message: " + response);
