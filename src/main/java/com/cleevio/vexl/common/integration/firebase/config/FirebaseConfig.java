d8ff8dc34fcef70bb6d7e43161c2e217c3c76e1d
David Tilšer <davidtilser@gmail.com>
2022-11-21 11:57:42 +0100

    VEX-1412 Firebase.json file removed and its properties moved to k8s secret

--- src/main/java/com/cleevio/vexl/common/integration/firebase/config/FirebaseConfig.java
+++ src/main/java/com/cleevio/vexl/common/integration/firebase/config/FirebaseConfig.java
@@ -4,24 +4,31 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.json.JSONObject;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Slf4j
 @Configuration
 public class FirebaseConfig {
 
-    public FirebaseConfig(@Value("${firebase.config.path}") String configPath) {
-        if (configPath.isBlank()) {
-            return;
-        }
-
+    public FirebaseConfig(FirebaseProperties properties) {
         try {
+            JSONObject json = new JSONObject();
+            json.put("client_id", properties.clientId());
+            json.put("client_email", properties.clientEmail());
+            json.put("private_key", properties.privateKey());
+            json.put("private_key_id", properties.privateKeyId());
+            json.put("project_id", properties.projectId());
+            json.put("token_uri", properties.tokenUri());
+            json.put("type", properties.serviceType());
+
+            final var inputStream = new ByteArrayInputStream(json.toString().getBytes());
+
             var options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(configPath).getInputStream())).build();
+                    .setCredentials(GoogleCredentials.fromStream(inputStream)).build();
 
             if (FirebaseApp.getApps().isEmpty()) {
                 FirebaseApp.initializeApp(options);
